a277c5ffcea31dc8194f2f1ffafe4d87998436b0
Charlotte Rushen <charlotterushen@hotmail.com>
2019-06-02 11:55:50 +0100

    Update to nav

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
- const pageTop = document.querySelector('#home');
- 
- const scroll = new SmoothScroll('a[href*="#"]', {
-  easing: 'easeInOutQuint',
-  speed: 700,
-  speedAsDuration: true,
-  clip: true,
+const scroll = new SmoothScroll('a[href*="#"]', {
+   easing: 'easeInOutQuint',
+   speed: 700,
+   speedAsDuration: true,
+   clip: true,
    offset: 10 
-});
+ });
+ 
