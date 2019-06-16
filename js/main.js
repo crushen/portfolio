1928fc50f774e5135b266d648f69e6267d85a263
Charlotte Rushen <charlotterushen@hotmail.com>
2019-06-16 15:48:05 +0100

    Adding animation to skills boxes

--- js/main.js
+++ js/main.js
@@ -2,6 +2,19 @@ const scroll = new SmoothScroll('a[href*="#"]', {
    easing: 'easeInOutQuint',
    speed: 700,
    speedAsDuration: true,
-   clip: true,
-  offset: 15
+   clip: true
  });
+
+ const skillsBox = document.querySelectorAll('.skills-box');
+ const line = document.querySelectorAll('.line');
+
+for (let i = 0; i < skillsBox.length; i++) {
+   skillsBox[i].addEventListener('mouseover', () => {
+      line[i].classList.add('line-expand');
+   });
+}
+
+for (let i = 0; i < skillsBox.length; i++) {
+   skillsBox[i].addEventListener('mouseout', () => {
+      line[i].classList.remove('line-expand');
+   });
