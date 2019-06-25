7c4eae1880d3616aff6ea4e2ff36bcb74e31011e
Charlotte Rushen <charlotterushen@hotmail.com>
2019-06-25 15:45:51 +0100

    Portfolio template and refactoring

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ const scroll = new SmoothScroll('a[href*="#"]', {
 
  const skillsBox = document.querySelectorAll('.skills-box');
  const line = document.querySelectorAll('.line');
+ const portfolioItem = document.querySelectorAll('.p-link');
+ const pLine = document.querySelectorAll('.p-line');
+
 
 for (let i = 0; i < skillsBox.length; i++) {
    skillsBox[i].addEventListener('mouseover', () => {
@@ -17,5 +20,17 @@ for (let i = 0; i < skillsBox.length; i++) {
 for (let i = 0; i < skillsBox.length; i++) {
    skillsBox[i].addEventListener('mouseout', () => {
       line[i].classList.remove('line-expand');
+   });
+}
+
+for (let i = 0; i < portfolioItem.length; i++) {
+   portfolioItem[i].addEventListener('mouseover', () => {
+      pLine[i].classList.add('line-expand');
+   });
+}
+
+for (let i = 0; i < portfolioItem.length; i++) {
+   portfolioItem[i].addEventListener('mouseout', () => {
+      pLine[i].classList.remove('line-expand');
    });
 }
